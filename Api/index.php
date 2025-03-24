d0250599e3f977c77ace8b3ac103f86e7016eb94
Mustafa <62420049+Mustafa5361@users.noreply.github.com>
2025-03-24 15:10:27 +0300

    Proje Yüklemesi

---
+++ Api/index.php
@@ -0,0 +1,21 @@
+<?php
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+
+    $value = $_POST["value"];
+
+    $value = json_decode($value);
+
+    if($value -> usermame == "deneme" && $value -> "deneme")
+    {
+        echo json_encode(["success" => true, "message" => "login successful"]);
+    }
+    else
+    {
+        echo json_encode(["success" => false, "message" => "username or password is incorrect"]);
+    }
+
+}
+
+?>
